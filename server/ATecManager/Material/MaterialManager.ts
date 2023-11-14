6fc3dc841619729b0c5ecb629c6814e3b1544046
Barsch2006 <christian0511@gmx.de>
2023-11-14 10:20:07 +0100

    material manager

--- server/ATecManager/Material/MaterialManager.ts
+++ server/ATecManager/Material/MaterialManager.ts
@@ -1,5 +1,67 @@
+import { Collection } from "mongodb";
+import ATecManager from "../ATecManager";
+import {
+    DefectTypes,
+    LocationType,
+    MaterialItem,
+    MaterialItemClassification,
+    MaterialItemType,
+    OwnerType,
+} from "./MaterialTypes";
+
 class MaterialManager {
-    // todo
+    private static _instance?: MaterialManager;
+    private itemCollection: Collection<MaterialItem>;
+    private typesCollection: Collection<MaterialItemType>;
+
+    static get instance(): MaterialManager | undefined {
+        return MaterialManager._instance;
+    }
+
+    private constructor(
+        coll: Collection<MaterialItem>,
+        types: Collection<MaterialItemType>,
+    ) {
+        if (!MaterialManager._instance) MaterialManager._instance = this;
+        this.itemCollection = coll;
+        this.typesCollection = types;
+    }
+
+    static async init(): Promise<MaterialManager> {
+        const manager = ATecManager.instance
+            ? ATecManager.instance
+            : await ATecManager.init(useRuntimeConfig());
+        const coll = manager.database.collection<MaterialItem>("material-items");
+        const types = manager.database.collection<MaterialItemType>("material-types");
+        return new MaterialManager(coll, types);
+    }
+
+    async addMaterialItem(item: MaterialItem): Promise<boolean> {
+        const result = await this.itemCollection.insertOne(item);
+        return result.acknowledged;
+    }
+
+    async addMaterialItemType(type: MaterialItemType): Promise<boolean> {
+        const result = await this.typesCollection.insertOne(type);
+        return result.acknowledged;
+    }
+
+    async getMaterialItemTypes(): Promise<MaterialItemType[]> {
+        const result = await this.typesCollection.find().toArray();
+        return result;
+    }
+
+    async searchMaterialItems(query: {
+        manufacturer?: string;
+        product_name?: string;
+        classification?: MaterialItemClassification;
+        location?: LocationType;
+        owner?: OwnerType;
+        defect?: DefectTypes;
+    }): Promise<MaterialItem[]> {
+        const result = await this.itemCollection.find(query).toArray();
+        return result;
+    }
 }
 
 export default { MaterialManager };
--- server/ATecManager/Material/MaterialTypes.ts
+++ server/ATecManager/Material/MaterialTypes.ts
@@ -64,4 +64,5 @@ export {
     OwnerType,
     MaterialItemType,
     MaterialItemClassification,
+    DefectTypes,
 };
