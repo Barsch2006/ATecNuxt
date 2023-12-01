fb2042eae92bcd20afa07419f7ad15b37774bb40
Barsch2006 <christian0511@gmx.de>
2023-12-01 20:55:38 +0100

    added event class

--- server/ATecManager/Events/Event.ts
+++ server/ATecManager/Events/Event.ts
@@ -1,4 +1,4 @@
-import { ObjectId } from "mongodb";
+import { ObjectId, WithId } from "mongodb";
 
 interface IEquipment {
     type: string;
@@ -36,4 +36,45 @@ interface IEvent {
     };
 }
 
+class Event implements WithId<IEvent> {
+    _id: ObjectId;
+    veranstalter: {
+        userid: ObjectId;
+        fullName: string;
+        email: string;
+    };
+    general: {
+        title: string;
+        description: string;
+        start: number; // start datetime in unix timestamp
+        end: number; // end datetime in unix timestamp
+        location: string | string[];
+        fileurls: string[];
+    };
+    technical: {
+        equipment: IEquipment[];
+        equipmentNotes?: string;
+        planungsurl: string;
+    };
+    notes?: string;
+    status?: {
+        technicians?: {
+            userid: ObjectId;
+            name: string;
+            email: string;
+        }[];
+        technicianNotes?: string[];
+    };
+
+    constructor(event: WithId<IEvent>) {
+        this._id = event._id;
+        this.veranstalter = event.veranstalter;
+        this.general = event.general;
+        this.technical = event.technical;
+        this.notes = event.notes;
+        this.status = event.status;
+    }
+}
+
+export default Event;
 export { IEvent, IEquipment };
