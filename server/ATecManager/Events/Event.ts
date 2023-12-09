914cbbc0338e92008db1775024976cbfb0b446a0
Barsch2006 <christian0511@gmx.de>
2023-12-09 16:22:57 +0100

    Refactor Event class and add new methods

--- server/ATecManager/Events/Event.ts
+++ server/ATecManager/Events/Event.ts
@@ -1,11 +1,11 @@
 import { ObjectId, WithId } from "mongodb";
 import ATecManager from "../ATecManager";
 import MethodResult, { ErrorTypes } from "../MethodResult";
+import User from "../User/User";
 
 interface IEquipment {
     type: string;
     amount: number;
-    notes?: string;
 }
 
 interface IEvent {
@@ -24,17 +24,17 @@ interface IEvent {
     };
     technical: {
         equipment: IEquipment[];
-        equipmentNotes?: string;
+        equipmentNote?: string;
         planungsurl: string;
     };
     notes?: string;
-    status?: {
-        technicians?: {
+    status: {
+        technicians: {
             userid: ObjectId;
             name: string;
             email: string;
         }[];
-        technicianNotes?: string[];
+        technicianNotes: string[];
     };
 }
 
@@ -90,17 +90,17 @@ class Event implements WithId<IEvent> {
     };
     technical: {
         equipment: IEquipment[];
-        equipmentNotes?: string;
+        equipmentNote?: string;
         planungsurl: string;
     };
     notes?: string;
-    status?: {
-        technicians?: {
+    status: {
+        technicians: {
             userid: ObjectId;
             name: string;
             email: string;
         }[];
-        technicianNotes?: string[];
+        technicianNotes: string[];
     };
 
     constructor(event: WithId<IEvent>) {
@@ -110,6 +110,108 @@ class Event implements WithId<IEvent> {
         this.technical = event.technical;
         this.notes = event.notes;
         this.status = event.status;
+    }
+
+    async addTechnician(user: User): Promise<MethodResult<boolean>> {
+        if (this.status.technicians.find((tech) => tech.userid.equals(user._id))) {
+            return [undefined, ErrorTypes.ALREADY_EXISTS];
+        } else {
+            this.status.technicians.push({
+                userid: user._id,
+                name: user.fullName,
+                email: user.email,
+            });
+            // update the event in the database
+            const atec =
+                ATecManager.instance ?? (await ATecManager.init(useRuntimeConfig()));
+            const eventCollection = atec.database.collection<IEvent>("events");
+            const result = await eventCollection.updateOne(
+                { _id: this._id },
+                { $push: { technicians: { userid: user._id } } },
+            );
+            return [result.acknowledged, null];
+        }
+    }
+
+    async removeTechnician(user: User): Promise<MethodResult<boolean>> {
+        const index = this.status.technicians.findIndex((tech) =>
+            tech.userid.equals(user._id),
+        );
+        if (index === -1) {
+            return [undefined, ErrorTypes.NOT_FOUND];
+        } else {
+            this.status.technicians.splice(index, 1);
+            // update the event in the database
+            const atec =
+                ATecManager.instance ?? (await ATecManager.init(useRuntimeConfig()));
+            const eventCollection = atec.database.collection<IEvent>("events");
+            const result = await eventCollection.updateOne(
+                { _id: this._id },
+                { $pull: { technicians: { userid: user._id } } },
+            );
+            return [result.acknowledged, null];
+        }
+    }
+
+    async addTechnicianNote(user: User, note: string): Promise<MethodResult<boolean>> {
+        const index = this.status.technicians.findIndex((tech) =>
+            tech.userid.equals(user._id),
+        );
+        if (index === -1) {
+            return [undefined, ErrorTypes.NOT_FOUND];
+        } else {
+            this.status.technicianNotes[index] = note;
+            // update the event in the database
+            const atec =
+                ATecManager.instance ?? (await ATecManager.init(useRuntimeConfig()));
+            const eventCollection = atec.database.collection<IEvent>("events");
+            const result = await eventCollection.updateOne(
+                { _id: this._id },
+                { $set: { [`technicianNotes.${index}`]: note } },
+            );
+            return [result.acknowledged, null];
+        }
+    }
+
+    async addEquipment(equipment: IEquipment): Promise<MethodResult<boolean>> {
+        this.technical.equipment.push(equipment);
+        // update the event in the database
+        const atec = ATecManager.instance ?? (await ATecManager.init(useRuntimeConfig()));
+        const eventCollection = atec.database.collection<IEvent>("events");
+        const result = await eventCollection.updateOne(
+            { _id: this._id },
+            { $push: { equipment: equipment } },
+        );
+        return [result.acknowledged, null];
+    }
+
+    async removeEquipment(index: number): Promise<MethodResult<boolean>> {
+        if (index >= this.technical.equipment.length) {
+            return [undefined, ErrorTypes.NOT_FOUND];
+        } else {
+            this.technical.equipment.splice(index, 1);
+            // update the event in the database
+            const atec =
+                ATecManager.instance ?? (await ATecManager.init(useRuntimeConfig()));
+            const eventCollection = atec.database.collection<IEvent>("events");
+            const result = await eventCollection.updateOne(
+                { _id: this._id },
+                { $pull: { equipment: { index: index } } },
+            );
+            return [result.acknowledged, null];
+        }
+    }
+
+    async updateGeneralInfo(general: IEvent["general"]): Promise<MethodResult<boolean>> {
+        this.general = general;
+        // update the event in the database
+        const atec = ATecManager.instance ?? (await ATecManager.init(useRuntimeConfig()));
+        const eventCollection = atec.database.collection<IEvent>("events");
+        const result = await eventCollection.updateOne(
+            { _id: this._id },
+            { $set: { general: general } },
+        );
+        return [result.acknowledged, null];
     }
 }
 
